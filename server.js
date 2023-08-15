2c7ce41227516efe91ea270fd1c00f015298988e
Harshit-1309 <harshitranjan1812@gmail.com>
2023-08-16 03:49:10 +0530

    Landing Page Routing

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
+// landing page middleware routes
+app.use(express.static("Frontend"));
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
